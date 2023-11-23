2dcda084e6bb4aed7a36bd13831a8928e1f37fac
daizhegnxin224 <daizhengxin224@hellobike.com>
2023-11-23 14:37:41 +0800

    feat: 第一章，实现一个简单的bean容器

---
+++ common/src/main/java/org/spring/common/service/UserService.java
@@ -0,0 +1,22 @@
+package org.spring.common.service;
+
+import org.spring.common.domain.User;
+
+import java.util.List;
+
+/**
+ * 用户服务
+ *
+ * @author zhengxin
+ * @since 1.0.0
+ */
+public interface UserService {
+
+    /**
+     * 获取用户列表
+     *
+     * @return {@code List<User> }
+     */
+    List<User> listUser();
+
+}
